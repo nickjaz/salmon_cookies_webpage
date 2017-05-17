2e339388501b014b1768a975c9870f732ed7e822
Nick Asmussen <nick.asmussen1@gmail.com>
2017-05-16 19:20:02 -0700

    finished the js and now moved on to re-style the data table and add comments

--- app.js
+++ app.js
@@ -1,59 +1,41 @@
 'use strict';
-//
-// var timesOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm:', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
 
-// function createList(location) {
-//   var container = document.createElement('div'); // make a container
-//   container.innerHTML = '<h2>' + location.name + '</h2>';
-//   document.body.appendChild(container);
-//
-//   var pairsList = []; // combining the the location sales array with the time of day array
-//
-//   for (var k = 0; k < 16; k++) {
-//     pairsList.push(timesOfDay[k] + location.salesByHour()[k] + ' cookies');
-//   }
-//
-//   var list= document.createElement('ul'); // create the list html
-//   var listArr = [];
-//
-//   for (var m = 0; m < 16; m++) { //loops the list array to
-//     listArr.push('<li>' + pairsList[m] + '</li>');
-//   }
-//
-//   var full_list = listArr.join('');
-//   list.innerHTML = full_list;
-//   document.body.appendChild(list);
-// }
+var thead = document.getElementById('thead');
+var headValues = ['','6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Location Total'];
+
+var newRow;
+
+for (var a = 0; a < headValues.length; a++) {
+  newRow = document.createElement('td');
+  newRow.innerHTML = headValues[a];
+  thead.appendChild(newRow);
+}
 
 function Store(name, minCust, maxCust, avgSales) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgSales = avgSales;
+  this.hourlySales = [];
 }
 
 Store.prototype.custPerHour = function() {
   var customers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-  console.log(customers);
   return customers;
 };
 
 Store.prototype.salesByHour = function() { //an array of sales by the hour ranging 14 hours
-  var hourlySales = [];
   for (var i = 0; i < 14; i++) {
     var sales = this.custPerHour() * this.avgSales;
-    hourlySales.push(Math.ceil(sales));
+    this.hourlySales.push(Math.ceil(sales));
   }
   var totalSales = 0; //adds the sales per hour together with for loop
-  for (var j = 0; j < 14; j++) {
-    totalSales = hourlySales[j] + totalSales;
+  for (var j = 0; j < this.hourlySales.length; j++) {
+    totalSales = this.hourlySales[j] + totalSales;
   }
-  hourlySales.push(totalSales); //pushes total to end of the array
-  // console.log(hourlySales);
-  return hourlySales;
+  this.hourlySales.push(totalSales); //pushes total to end of the array
+  return this.hourlySales;
 };
-
-
 
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
@@ -63,33 +45,43 @@ var alki = new Store('Alki', 2, 16, 4.6);
 
 var locations = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
 
+for (var k = 0; k < locations.length; k++) {
+  locations[k].salesByHour();
+}
+
 var table = document.getElementById('shell');
 var data = [];
 
-for (var k = 0; k < locations.length; k++) {
-  data.push(
-    '<td>' + locations[k].name + '</td>' +
-    '<td>' + locations[k].salesByHour()[0] + '</td>' +
-    '<td>' + locations[k].salesByHour()[1] + '</td>' +
-    '<td>' + locations[k].salesByHour()[2] + '</td>' +
-    '<td>' + locations[k].salesByHour()[3] + '</td>' +
-    '<td>' + locations[k].salesByHour()[4] + '</td>' +
-    '<td>' + locations[k].salesByHour()[5] + '</td>' +
-    '<td>' + locations[k].salesByHour()[6] + '</td>' +
-    '<td>' + locations[k].salesByHour()[7] + '</td>' +
-    '<td>' + locations[k].salesByHour()[8] + '</td>' +
-    '<td>' + locations[k].salesByHour()[9] + '</td>' +
-    '<td>' + locations[k].salesByHour()[10] + '</td>' +
-    '<td>' + locations[k].salesByHour()[11] + '</td>' +
-    '<td>' + locations[k].salesByHour()[12] + '</td>' +
-    '<td>' + locations[k].salesByHour()[13] + '</td>' +
-    '<td>' + locations[k].salesByHour()[14] + '</td>');
+for (var m = 0; m < locations.length; m++) {
+  var dataList = '<td>' + locations[m].name + '</td>';
+  for (var n = 0; n < 15; n++) {
+    dataList = dataList + '<td>' + locations[m].hourlySales[n] + '</td>';
+  }
+  data.push(dataList);
 }
 
-var newRow;
+console.log(dataList);
 
-for (var m = 0; m < data.length; m++) {
+var grandTotal = [];
+for (var x = 0; x <15; x++) {
+  grandTotal.push(
+    locations[0].hourlySales[x] +
+    locations[1].hourlySales[x] +
+    locations[2].hourlySales[x] +
+    locations[3].hourlySales[x] +
+    locations[4].hourlySales[x]
+  );
+}
+
+var grandDataList = '<td>' + 'Totals' + '</td>';
+for (var y = 0; y < 15; y++) {
+  grandDataList = grandDataList + '<td>' + grandTotal[y] + '</td>';
+}
+
+data.push(grandDataList);
+
+for (var z = 0; z < data.length; z++) {
   newRow = document.createElement('tr');
-  newRow.innerHTML = data[m];
+  newRow.innerHTML = data[z];
   table.appendChild(newRow);
 }
