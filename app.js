5c16530b87d9cd7672b0496feb835a1743fde7da
Nick Asmussen <nick.asmussen1@gmail.com>
2017-05-16 20:00:00 -0700

    added comments to js file

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 'use strict';
 
+//created the table header here
 var thead = document.getElementById('thead');
 var headValues = ['','6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Location Total'];
 
+// creates the newRow to be used to build the DOM
 var newRow;
 
+//loops the headValues in the array above and puts them in the DOM table head
 for (var a = 0; a < headValues.length; a++) {
   newRow = document.createElement('td');
   newRow.innerHTML = headValues[a];
   thead.appendChild(newRow);
 }
 
+//constructor function
 function Store(name, minCust, maxCust, avgSales) {
   this.name = name;
   this.minCust = minCust;
@@ -19,17 +23,21 @@ function Store(name, minCust, maxCust, avgSales) {
   this.hourlySales = [];
 }
 
+//prototype chain methods
 Store.prototype.custPerHour = function() {
   var customers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   return customers;
 };
 
-Store.prototype.salesByHour = function() { //an array of sales by the hour ranging 14 hours
+//an array of sales by the hour ranging 14 hours
+Store.prototype.salesByHour = function() {
   for (var i = 0; i < 14; i++) {
     var sales = this.custPerHour() * this.avgSales;
     this.hourlySales.push(Math.ceil(sales));
   }
-  var totalSales = 0; //adds the sales per hour together with for loop
+
+  //adds the sales per hour together with the loop
+  var totalSales = 0;
   for (var j = 0; j < this.hourlySales.length; j++) {
     totalSales = this.hourlySales[j] + totalSales;
   }
@@ -37,18 +45,21 @@ Store.prototype.salesByHour = function() { //an array of sales by the hour rangi
   return this.hourlySales;
 };
 
+// the store objects pass in their arguments
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
+//array of the store loactions to be used in DOM building loops
 var locations = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
 
 for (var k = 0; k < locations.length; k++) {
   locations[k].salesByHour();
 }
 
+//filling out the bulk of the table by looping the objects values and assigning them to a data array
 var table = document.getElementById('shell');
 var data = [];
 
@@ -60,8 +71,7 @@ for (var m = 0; m < locations.length; m++) {
   data.push(dataList);
 }
 
-console.log(dataList);
-
+//calculating the grand total of all store sales for each an hour
 var grandTotal = [];
 for (var x = 0; x <15; x++) {
   grandTotal.push(
@@ -73,13 +83,14 @@ for (var x = 0; x <15; x++) {
   );
 }
 
+//assembling the grand total for the table
 var grandDataList = '<td>' + 'Totals' + '</td>';
 for (var y = 0; y < 15; y++) {
   grandDataList = grandDataList + '<td>' + grandTotal[y] + '</td>';
 }
-
 data.push(grandDataList);
 
+//placing all the data collected in the data array into the DOM
 for (var z = 0; z < data.length; z++) {
   newRow = document.createElement('tr');
   newRow.innerHTML = data[z];
